64dc3524dd2903f3ff491619e3be9021604dec28
Vladyslav Yakovlev <Vlad@macbook-air.local>
2018-01-22 15:08:53 +0200

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Vladyslav Yakovlev on 22.01.2018.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
